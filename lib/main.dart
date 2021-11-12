d6f0acf23dfdfbc26f794579924bdcfadebda498
Hanuk0 <gushanhaj@student.gu.se>
2021-11-12 14:32:20 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MainView(),
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+    );
+  }
+}
+
+class MainView extends StatelessWidget {
+  const MainView({Key? key}) : super(key: key);
+
+  get colors => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('TIG169 TODO'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SecondView()));
+            },
+          ),
+        ],
+      ),
+      body: const Center(),
+    );
+  }
+}
+
+class SecondView extends StatelessWidget {
+  const SecondView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
