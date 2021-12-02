28b6098e28cf591d1bafc58e0a7321e39bc20fda
Hanuk0 <gushanhaj@student.gu.se>
2021-12-02 13:17:46 +0100

    Lagt till kommentarer

--- lib/model.dart
+++ lib/model.dart
@@ -36,33 +36,39 @@ class MyState extends ChangeNotifier {
   List<ToDoItem> get list => _list;
   int get filterBy => _filterBy;
 
+//hämtar lista med befintliga tasks från Api
   Future getList() async {
     List<ToDoItem> list = await Api.getToDos();
     _list = list;
     notifyListeners();
   }
 
+//filtrering för 'All', 'Done' och 'Undone' tasks
   void setFilterBy(int filterBy) {
     _filterBy = filterBy;
     notifyListeners();
   }
 
+//lägger till task i Api och lokalt
   void addTask(ToDoItem task) async {
     _list = await Api.addTask(task);
     notifyListeners();
   }
 
+//Tar bort task från Api och lokalt
   void removeTask(ToDoItem task) async {
     _list = await Api.deleteTask(task.id);
     notifyListeners();
   }
 
+//Uppdatera status på task när checkbox bockas i/ur
   void changedTask(ToDoItem task) async {
     task.changedCB(task);
     _list = await Api.changeTask(task.id, task);
     notifyListeners();
   }
 
+//Uppdatera status till 'Done' när checkbox är ibockad
   void isDone(ToDoItem task) async {
     task.changedCB(task);
     notifyListeners();
